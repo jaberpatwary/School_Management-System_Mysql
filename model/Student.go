f30f5dcda12ced5279a5d7e5084718fa3a84d75f
jaber patwary <jaberpatwary0@gamil.com>
2025-05-26 07:17:47 +0600

    change

--- model/Student.go
+++ model/Student.go
@@ -4,13 +4,13 @@ type Student struct {
 	ID            int     `json:"id"`
 	Name          string  `json:"name"`
 	Date_Of_Birth *int    `json:"date_of_birth"`
-	Email         *string `json:"Email"`
+	Email         *string `json:"email"`
 }
 
 type PostStudent struct {
 	Name          string  `json:"name"`
 	Date_Of_Birth *int    `json:"date_of_birth"`
-	Email         *string `json:"Email"`
+	Email         *string `json:"email"`
 }
 
 type StudentUri struct {
